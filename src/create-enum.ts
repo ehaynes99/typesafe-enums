b82c353c3d7a4a6fe475240b75492db1296720f7
Eric Haynes <ehaynes99@gmail.com>
2022-11-14 16:51:50 -0500

    Improve type safety and add additional utility methods

--- src/create-enum.ts
+++ src/create-enum.ts
@@ -1,14 +1,24 @@
-type ValidKeys = Exclude<string, 'isKey' | 'isValue'>
+type InvalidKeys = keyof ReturnType<typeof createMethods<any>>
 
 type EnumValues =
-  | Record<ValidKeys, string>
-  | Record<ValidKeys, number>
-  | Record<ValidKeys, symbol>
+  | Record<string, string>
+  | Record<string, number>
+  | Record<string, symbol>
 
-export type Enum<Values extends EnumValues> = Values & {
-  isKey(maybeKey: unknown): maybeKey is keyof Values
-  isValue(maybeValue: unknown): maybeValue is Values[keyof Values]
+/**
+ * Prevents using the same value twice under different keys.
+ * Prevents using any of the enum's method names as keys
+ */
+type ValidValues<T extends EnumValues> = {
+  [K in keyof T]: T[K] extends T[Exclude<keyof T, K>]
+    ? 'All values must be unique'
+    : K extends InvalidKeys
+    ? 'Keys can not include Enum method names'
+    : T[K]
 }
+
+export type Enum<Values extends EnumValues> = Values &
+  ReturnType<typeof createMethods<Values>>
 
 export type EnumKey<E extends Enum<any>> = E extends Enum<infer Values>
   ? keyof Values
@@ -18,21 +28,62 @@ export type EnumValue<E extends Enum<any>> = E extends Enum<infer Values>
   ? Values[keyof Values]
   : never
 
+const createMethods = <V extends Record<string, any>>(values: V) => {
+  values = { ...values }
+  const enumKeys = new Set<keyof V>(Object.keys(values))
+  const enumValues = new Set<V[keyof V]>(Object.values(values))
+
+  if (enumValues.size !== enumKeys.size) {
+    throw new TypeError('Enumerated values must be unique')
+  }
+
+  const reverseMapping = Object.fromEntries(
+    Object.entries(values).map(([key, value]) => [value, key]),
+  )
+
+  const methods = {
+    keys: () => [...enumKeys],
+    values: () => [...enumValues],
+    isKey: (maybeKey: unknown): maybeKey is keyof V => {
+      return enumKeys.has(maybeKey as any)
+    },
+    isValue: (maybeValue: unknown): maybeValue is V[keyof V] => {
+      return enumValues.has(maybeValue as any)
+    },
+    keyOf: (value: V[keyof V]): keyof V => {
+      return reverseMapping[value]
+    },
+  }
+
+  for (const methodName in methods) {
+    if (enumKeys.has(methodName)) {
+      throw new TypeError(
+        `Names of enum methods are reserved: ${Object.keys(methods).sort}`,
+      )
+    }
+  }
+  return methods
+}
+
 export const createEnum = <Values extends EnumValues>(
-  values: Values,
+  values: ValidValues<Values>,
 ): Enum<Values> => {
   values = { ...values }
-  const enumKeys = new Set(Object.keys(values))
-  const enumValues = new Set(Object.values(values))
+  const methods = createMethods(values)
 
   return Object.freeze(
-    Object.assign(values, {
-      isKey(maybeKey: unknown): maybeKey is keyof Values {
-        return enumKeys.has(maybeKey as any)
-      },
-      isValue(maybeValue: unknown): maybeValue is Values[keyof Values] {
-        return enumValues.has(maybeValue as any)
-      },
-    }),
-  )
+    Object.defineProperties(
+      values,
+      Object.entries(methods).reduce<Record<string, PropertyDescriptor>>(
+        (result, [name, method]) => {
+          result[name] = {
+            enumerable: false,
+            value: method,
+          }
+          return result
+        },
+        {},
+      ),
+    ),
+  ) as Enum<Values>
 }
--- test/create-enum.test.ts
+++ test/create-enum.test.ts
@@ -1,37 +1,77 @@
 import { createEnum } from '../src/create-enum'
 
 describe('Enum', () => {
-  it('has values from original', async () => {
-    const Test = createEnum({
-      first: 'one',
-      second: 'two',
-      third: 'three',
-    })
+  const TestEnum = createEnum({
+    first: 'one',
+    second: 'two',
+    third: 'three',
+  })
 
-    expect(Test.first).toBe('one')
-    expect(Test.second).toBe('two')
-    expect(Test.third).toBe('three')
+  it('has values from original', async () => {
+    expect(TestEnum.first).toBe('one')
+    expect(TestEnum.second).toBe('two')
+    expect(TestEnum.third).toBe('three')
   })
 
   it('checks if random value is a key', async () => {
-    const Test = createEnum({
-      first: 'one',
-      second: 'two',
-      third: 'three',
-    })
-
-    expect(Test.isKey('first')).toBe(true)
-    expect(Test.isKey('seventh')).toBe(false)
+    expect(TestEnum.isKey('first')).toBe(true)
+    expect(TestEnum.isKey('seventh')).toBe(false)
   })
 
   it('checks if random value is a value', async () => {
-    const Test = createEnum({
-      first: 'one',
-      second: 'two',
-      third: 'three',
-    })
+    expect(TestEnum.isValue('one')).toBe(true)
+    expect(TestEnum.isValue('seven')).toBe(false)
+  })
 
-    expect(Test.isValue('one')).toBe(true)
-    expect(Test.isValue('seven')).toBe(false)
+  it('performs a reverse lookup of values', async () => {
+    expect(TestEnum.keyOf('one')).toBe('first')
+    expect(TestEnum.keyOf('two')).toBe('second')
+    expect(TestEnum.keyOf('three')).toBe('third')
+  })
+
+  it('enumerates the keys', async () => {
+    const keys = TestEnum.keys()
+    expect(keys).toEqual(['first', 'second', 'third'])
+  })
+
+  it('enumerates the values', async () => {
+    const values = TestEnum.values()
+    expect(values).toEqual(['one', 'two', 'three'])
+  })
+
+  it('has only the enum keys as enumerable properties', async () => {
+    const keys = Object.keys(TestEnum)
+    expect(keys).toEqual(['first', 'second', 'third'])
+  })
+
+  it('has only the enum values as enumerable properties', async () => {
+    const keys = Object.values(TestEnum)
+    expect(keys).toEqual(['one', 'two', 'three'])
+  })
+
+  it('throws an error if keys include and enum method name', async () => {
+    expect(() => {
+      createEnum({
+        // @ts-expect-error restricted method name as key
+        keys: 'keys-value',
+        // @ts-expect-error restricted method name as key
+        values: 'values-value',
+        // @ts-expect-error restricted method name as key
+        isKey: 'isKey value',
+        // @ts-expect-error restricted method name as key
+        isValue: 'isValue value',
+      })
+    }).toThrow(TypeError)
+  })
+
+  it('throws an error if values are not unique', async () => {
+    expect(() => {
+      createEnum({
+        // @ts-expect-error duplicate values
+        first: 'one',
+        // @ts-expect-error duplicate values
+        second: 'one',
+      })
+    }).toThrow(new TypeError('Enumerated values must be unique'))
   })
 })
